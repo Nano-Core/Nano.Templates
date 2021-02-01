b028fe0ae7086ed9f90bb8f6d5bf84fe3612a6f6
vivet <vivet@vivetonline.com>
2021-02-01 14:11:36 +0100

    Minor fixes

---
+++ Console/Nano.Template.Console/Data/ConsoleDbContextFactory.cs
@@ -0,0 +1,12 @@
+﻿using Nano.Data;
+using Nano.Data.Providers.SqlServer; 
+//using Nano.Data.Providers.MySql;
+
+namespace Nano.Template.Console.Data
+{
+    /// <inheritdoc />
+    public class ConsoleDbContextFactory : BaseDbContextFactory<SqlServerProvider /*MySqlProvider*/, ConsoleDbContext>
+    {
+
+    }
+}
