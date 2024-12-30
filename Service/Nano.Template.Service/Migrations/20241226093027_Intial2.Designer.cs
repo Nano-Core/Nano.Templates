af1f1fa3cad5929590cb19c3dda4fa214b807a95
vivet <vivet@vivetonline.com>
2024-12-30 16:53:27 +0100

    Updated to latest Nano

--- Service/Nano.Template.Service/Migrations/20241226093027_Intial2.Designer.cs
+++ Service/Nano.Template.Service/Migrations/20241226093027_Intial2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Nano.Template.Service.Data;
 
@@ -11,9 +12,11 @@ using Nano.Template.Service.Data;
 namespace Nano.Template.Service.Migrations
 {
     [DbContext(typeof(ServiceDbContext))]
-    partial class ServiceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241226093027_Intial2")]
+    partial class Intial2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Service/Nano.Template.Service/Migrations/20241226093027_Intial2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nano.Template.Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class Intial2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "__EFAuthApiKey",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    IdentityUserId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Name = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Hash = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: false),
+                    ExpireAt = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: true),
+                    RevokedAt = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK___EFAuthApiKey", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK___EFAuthApiKey___EFAuthUser_IdentityUserId",
+                        column: x => x.IdentityUserId,
+                        principalTable: "__EFAuthUser",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX___EFAuthApiKey_ExpireAt",
+                table: "__EFAuthApiKey",
+                column: "ExpireAt");
+
+            migrationBuilder.CreateIndex(
+                name: "IX___EFAuthApiKey_IdentityUserId",
+                table: "__EFAuthApiKey",
+                column: "IdentityUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX___EFAuthApiKey_RevokedAt",
+                table: "__EFAuthApiKey",
+                column: "RevokedAt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "__EFAuthApiKey");
+        }
+    }
+}
