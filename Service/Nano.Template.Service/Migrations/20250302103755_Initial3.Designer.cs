1381a44bb85be80e035069ea17468b03c94f195d
vivet <vivet@vivetonline.com>
2025-03-02 11:53:10 +0100

    Added migration

--- Service/Nano.Template.Service/Migrations/20250302103755_Initial3.Designer.cs
+++ Service/Nano.Template.Service/Migrations/20250302103755_Initial3.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Nano.Template.Service.Data;
 
@@ -11,9 +12,11 @@ using Nano.Template.Service.Data;
 namespace Nano.Template.Service.Migrations
 {
     [DbContext(typeof(ServiceDbContext))]
-    partial class ServiceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250302103755_Initial3")]
+    partial class Initial3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Service/Nano.Template.Service/Migrations/20250302103755_Initial3.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nano.Template.Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX___EFAuthApiKey_ExpireAt",
+                table: "__EFAuthApiKey");
+
+            migrationBuilder.DropColumn(
+                name: "ExpireAt",
+                table: "__EFAuthApiKey");
+
+            migrationBuilder.AddColumn<string>(
+                name: "JsonMapped",
+                table: "Sample",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "__EFAuthUser",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NewEmail",
+                table: "__EFAuthUser",
+                type: "varchar(256)",
+                maxLength: 256,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NewPhoneNumber",
+                table: "__EFAuthUser",
+                type: "varchar(20)",
+                maxLength: 20,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JsonMapped",
+                table: "Sample");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "__EFAuthUser");
+
+            migrationBuilder.DropColumn(
+                name: "NewEmail",
+                table: "__EFAuthUser");
+
+            migrationBuilder.DropColumn(
+                name: "NewPhoneNumber",
+                table: "__EFAuthUser");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "ExpireAt",
+                table: "__EFAuthApiKey",
+                type: "datetime(6)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX___EFAuthApiKey_ExpireAt",
+                table: "__EFAuthApiKey",
+                column: "ExpireAt");
+        }
+    }
+}
